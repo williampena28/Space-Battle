8c9f0e879eb15bf86eeaafcb7c19e2d582fba027
williampena28 <56842242+williampena28@users.noreply.github.com>
2022-12-15 14:11:21 -0500

    added enemy ship class for a list of spaceships

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// main class for the space ship since they all have the same propertie
 class Spaceship
 {
     constructor(hull, firepower, accuracy)
@@ -6,11 +7,42 @@ class Spaceship
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
-
-
 };
 
+class enemyShips
+{
+    constructor(shipList) 
+    {
+        this.shipList = []; // enemy ship list to iterate through
+    }
+    addShip()
+    {
+        // hull value is 3 to 6, accuracy value is 2 to 4, and accuracy value is .6 to .8
+        let hull = Math.floor(Math.random() * (7 - 3) + 3);
+        let firepower = Math.floor(Math.random() * (7 - 3) + 3);
+        let accuracy = Math.round((Math.random() * (.81 - .6) + .6) * 10) / 10;
+        this.shipList.push(new Spaceship(hull, firepower, accuracy));
+    }
+}
+
+// alien fleet created and new ships have been put into the array
+let enemyFleet = new enemyShips();
+
+enemyFleet.addShip();
+enemyFleet.addShip();
+enemyFleet.addShip();
+enemyFleet.addShip();
+enemyFleet.addShip();
+enemyFleet.addShip();
+
 //Helper functions
+// ship destroyed
+const destroyShip = (enemyList) =>
+{
+    console.log(`Ship has been destroyed. Only ${enemyList.length} to go!`);
+    enemyList.shift();
+}
+
 // log game over
 const printGameOver = () =>
 {
@@ -22,45 +54,28 @@ const printWin = () =>
     return "YOU WIN!";
 }
 
-// returns a number between 3 and 6 using Math.floor() and Math.random()
-const giveHull = () =>
-{
-    return Math.floor(Math.random() * (7 - 3) + 3);
-}
-
-// returns a number between 2 and 4 using aforementioned functions
-const giveFirePower = () =>
-{
-    return Math.floor(Math.random() * (5 - 2) + 2);
-}
-
-// returns a float between .6 and .8
-const giveAccuracy = () =>
-{
-    return Math.round((Math.random() * (.81 - .6) + .6) * 10) / 10;
-}
-
 // our main spaceship
 let USS_HelloWorld = new Spaceship(20, 5, .7);
 
-// 6 alien ships 
-// hull, firepower, and accuracy values are all in random using the respective helper functions.
-let alien = 
-[
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy()),
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy()),
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy()),
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy()),
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy()),
-    new Spaceship(giveHull(), giveFirePower(), giveAccuracy())
-]
+//DOM
+// dom elements that grab the containers of both ships
+let myShip = document.getElementById('my-ship');
+let enemyShip = document.getElementById('enemy-ship');
 
-// flag variable for the main while loop that runs our game
-let terminate = false;
 
-// this is where the main game will run. When the game is over, stop the while loop until the page is refreshed
-while(!terminate)
+myShip.addEventListener('click', () =>
 {
-    console.log("test");
-    terminate = true;
-}
+    let myShipHull = document.getElementById('my-ship-hull');
+    myShipHull.innerHTML = `Hull: ${USS_HelloWorld.hull}`;
+})
+
+// main ship elements
+let myShipHull = document.getElementById('my-ship-hull');
+let myShipPower = document.getElementById('my-ship-firepower');
+let myShipAccuracy = document.getElementById('my-ship-accuracy');
+myShipHull.innerHTML = `Hull: ${USS_HelloWorld.hull}`
+
+// enemy ship elements
+let enemyShipHull = document.getElementById('enemy-ship-hull');
+let enemyShipPower = document.getElementById('enemy-ship-firewpower');
+let enemyShipAccuracy = document.getElementById('enemy-ship-accuracy');
